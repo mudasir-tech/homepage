d625c852b8bb94196e4c7cf6a0a20d5b814902b7
ansar-safdar123 <ansarsafdar143@gmail.com>
2025-10-15 21:48:07 +0500

    disable eslint during bui;ld

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // Ignore ESLint errors during production builds (like Vercel)
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
